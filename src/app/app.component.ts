9e13d96d2c5bf88cc0db3f78a991e7dc2d56040f
Kiran <tkedala@winsupplyinc.com>
2018-08-17 10:34:48 -0400

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Winsupply Main Menu';
+
+
+
+  gotoReceiving(){
+    window.open("http://localhost:8082/#/itembin-details","_self");
+    
+  }
+
+  gotoSoe(){
+    window.open("http://windev1:11011/profoundui/auth/genie?workstnid=TESTID&skin=wob&macro=SalesPersonInq&suffixid=1");
+  }
+
 }
